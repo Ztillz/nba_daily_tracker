ea6e1ba1bdbe9466eb5709da9ed81414074dbcae
ztillz <malcomrivard@outlook.com>
2025-05-18 18:13:24 -0400

    fixed fetch players

--- scripts/fetch_players.py
+++ scripts/fetch_players.py
@@ -4,7 +4,7 @@ from nba_api.stats.endpoints import leaguedashplayerstats
 import pandas as pd
 
 def fetch_top_scorers():
-    players_stats = leaguedashplayerstats.LeagueDashPlayersStats().get_data_frames()[0]
+    players_stats = leaguedashplayerstats.LeagueDashPlayerStats().get_data_frames()[0]
 
     #sort players by pts and keep only the top 100
     top_scorers = players_stats.sort_values(by='PTS', ascending=False).head(100)
