b886912bc71c5c5ee362ea1dac0de2324d98feeb
ztillz <malcomrivard@outlook.com>
2025-05-18 18:08:50 -0400

    added the fetch top 100 players to scripts and to actions

--- scripts/fetch_games.py
+++ scripts/fetch_games.py
@@ -1,13 +1,23 @@
 from nba_api.live.nba.endpoints import scoreboard
+from nba_api.stats.endpoints import leaguedashplayerstats
 import pandas as pd
 
 def fetch_today_games():
     games = scoreboard.ScoreBoard().games.get_dict()
-    print(games)
     df = pd.DataFrame(games)
-    print(df)
     df.to_csv("data/todays_games.csv", index=False)
     return df
+
+def fetch_top_scorers():
+    players_stats = leaguedashplayerstats.LeagueDashPlayersStats().get_data_frames()[0]
+
+    #sort players by pts and keep only the top 100
+    top_scorers = players_stats.sort_values(by='PTS', ascending=False).head(100)
+    
+    top_scorers.to_csv("data/top_scorers.csv", index=False)
+    print(top_scorers)
+    return top_scorers
+
 
 if __name__ == "__main__":
     fetch_today_games()
